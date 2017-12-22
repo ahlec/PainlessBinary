8453065883c8515b3f46b1c1f0285efbd3474874
Alec Deitloff <alec.deitloff@gmail.com>
2017-12-22 00:38:15 -0800

    GitKraken accidentally goofed and deleted a file from git momentarily.

---
+++ SonezakiMasaki/Containers/ListContainerDefinition.cs
@@ -0,0 +1,29 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------
+// SonezakiMasaki library project (https://github.com/ahlec/SonezakiMasaki/), a subproject of the Pokémon Kristall project.
+// This library is available to the public under the MIT license.
+// ------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+
+namespace SonezakiMasaki.Containers
+{
+    internal sealed class ListContainerDefinition : TypeDefinition
+    {
+        ListContainerDefinition( Type type, TypeDefinition contentTypeDefinition )
+        {
+            Type = type;
+            ContentTypeDefinition = contentTypeDefinition;
+        }
+
+        public Type Type { get; }
+
+        public TypeDefinition ContentTypeDefinition { get; }
+
+        public override ISerializableValue Instantiate( BinaryReader reader )
+        {
+            int listLength = reader.ReadInt32();
+            return new ListContainer( this, listLength );
+        }
+    }
+}
