6b0b64488fc153ce658663f2c7f14384bd02a6ee
Alec Deitloff <alec.deitloff@gmail.com>
2017-12-28 19:24:43 -0800

    Added BinaryExplorer, a tool that will come with SonezakiMasaki that lets you open a binary file and have it deserialize into a property grid and allows you to inspect the file and make changes to it from within there. Right now it's just an empty WPF project though.

---
+++ BinaryExplorer/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SonezakiMasaki;
+
+namespace BinaryExplorer
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TypeManager typeManager = new TypeManager();
+            Serializer serializer = new Serializer( typeManager );
+        }
+    }
+}
