d00171a6bb1f6678635b84538617eac19baad6b9
Alec Deitloff <alec.deitloff@gmail.com>
2017-12-30 09:30:21 -0800

    Added unit tests project in order to start getting some unit tests into the solution.

---
+++ SonezakiMasaki.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+// ------------------------------------------------------------------------------------------------------------------------
+// SonezakiMasaki library project (https://github.com/ahlec/SonezakiMasaki/), a subproject of the Pokémon Kristall project.
+// This library is available to the public under the MIT license.
+// ------------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "SonezakiMasaki.Tests" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "SonezakiMasaki.Tests" )]
+[assembly: AssemblyCopyright( "Copyright ©  2017" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "1b34b64d-8673-480f-92d7-4cb59f82e77c" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
